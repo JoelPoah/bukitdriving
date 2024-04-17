78e5156bbe1b7f3363eeaeee37bcb3b1b0b9dac4
JoelPoah <144647095+JoelPoah-Anor@users.noreply.github.com>
2024-04-17 09:36:54 +0800

    testing new shell

--- shell.sh
+++ shell.sh
@@ -9,12 +9,11 @@ run_python_script() {
 
     # Save the process ID of the Python script
     PYTHON_PID=$!
-
-    # Sleep for 7 minutes
-    sleep 420
+    CHROMEID1=$(pgrep -n "Google Chrome")
 
     # Kill the Python script`
     kill $PYTHON_PID
+    kill $CHROMEID1
     # echo "Python script killed"
     echo "Python script killed"
 
@@ -27,20 +26,10 @@ while true; do
     # Call the function to run the Python script
     run_python_script
 
-
-    # Get the process IDs (PIDs) of all Chrome instances
-    chrome_pids=$(pgrep -i "Google Chrome")
-
-    # Check if Chrome is running
-    if [ -z "$chrome_pids" ]; then
-      echo "Chrome is not running."
-    else
-      # Terminate all Chrome processes
-      echo "Closing Chrome..."
-      kill -15 $chrome_pids
-      echo "Chrome closed successfully."
-    fi
-
     # Sleep for 30sec before running again
-    sleep 30
+    sleep 360
 done
+
+
+
+
