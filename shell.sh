ed1e7a2c3e90b15359cdb02f721a695d8c76dffd
JoelPoah <144647095+JoelPoah-Anor@users.noreply.github.com>
2024-04-17 09:38:23 +0800

    error with shell

--- shell.sh
+++ shell.sh
@@ -6,17 +6,6 @@ run_python_script() {
     # C:/Users/Admin/Desktop/BBDC/venv/Scripts/activate
     # Run Python script in the background
     python3 main.py &
-
-    # Save the process ID of the Python script
-    PYTHON_PID=$!
-    CHROMEID1=$(pgrep -n "Google Chrome")
-
-    # Kill the Python script`
-    kill $PYTHON_PID
-    kill $CHROMEID1
-    # echo "Python script killed"
-    echo "Python script killed"
-
     #!/bin/bash
 
 }
@@ -25,9 +14,18 @@ run_python_script() {
 while true; do
     # Call the function to run the Python script
     run_python_script
+    # Save the process ID of the Python script
+    PYTHON_PID=$!
+    CHROMEID1=$(pgrep -n "Google Chrome")
+    sleep 360
+
+    # Kill the Python script`
+    kill $PYTHON_PID
+    kill $CHROMEID1
+    # echo "Python script killed"
+    echo "Python script killed"
 
     # Sleep for 30sec before running again
-    sleep 360
 done
 
 
